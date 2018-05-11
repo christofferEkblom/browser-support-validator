de45f31b31a0d5268deed18947f49223a151d1e9
christofferEkblom <cekmw09@student.lnu.se>
2018-05-11 18:17:02 +0200

    🔨 refactor

--- test/cssParserTest.js
+++ test/cssParserTest.js
@@ -1,5 +1,4 @@
 const chai = require('chai')
-let assert = chai.assert
 const sut = require('../app/cssParser')
 
 let css = '\
@@ -16,19 +15,23 @@ let css = '\
   }\
 '
 
+let uniqueProperties = 5
+let propertiesTotal = 6
+let assert = chai.assert
+
 describe('cssParser getProperties', () => {
   it('length should be 6 when duplicates is allowed', function() {
     let properties = sut.getProperties(css, {allowDuplicates: true})
-    assert.equal(properties.length, 6)
+    assert.equal(properties.length, propertiesTotal)
   })
 
   it('length should be 5 when duplicates is disallowed', function() {
     let properties = sut.getProperties(css, {allowDuplicates: false})
-    assert.equal(properties.length, 5)
+    assert.equal(properties.length, uniqueProperties)
   })
 
   it('should disallow duplicates by default if allowDuplicates option is not set', function() {
     let properties = sut.getProperties(css)
-    assert.equal(properties.length, 5)
+    assert.equal(properties.length, uniqueProperties)
   })
 })
