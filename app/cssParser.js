2b1dcf03e9027442420cd5dcfb70324e2135cde5
christofferEkblom <cekmw09@student.lnu.se>
2018-05-12 14:06:52 +0200

    🔨 refactor

--- app/cssParser.js
+++ app/cssParser.js
@@ -8,15 +8,11 @@ module.exports.getProperties = function(css, options) {
   cssObj.parse(css)['stylesheet']['rules'].forEach((rule) => {
     rule['declarations'].forEach((declaration) => {
       let property = declaration['property']
+      let optionsIsSet = typeof options !== 'undefined'
+      let propertyIsUnique = properties.indexOf(property) === -1
 
-      if(typeof options !== 'undefined' && options['allowDuplicates']) {
+      if(optionsIsSet && options['allowDuplicates'] || propertyIsUnique) {
         properties.push(property)
-      }
-
-      else {
-        if(properties.indexOf(property) === -1) {
-          properties.push(property)
-        }
       }
     })
   })
