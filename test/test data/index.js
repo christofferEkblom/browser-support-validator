b5ea490927e6db7269eafdc31acc359ab0c3a823
christofferEkblom <cekmw09@student.lnu.se>
2018-05-15 20:06:17 +0200

    🔨 new line

--- test/test data/index.js
+++ test/test data/index.js
@@ -3,6 +3,7 @@ module.exports = {
     'function testFile() {\n  let message = \'This code is not intended to be executed\'\n  alert(message);\n}\n',
     'console.log(\'This file is either not intended to be executed\')\n'
   ],
+
   css: '\
     body {\
       color: #fff;\
